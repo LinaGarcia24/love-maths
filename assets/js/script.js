ca6469f2265c358e13cfb4c8bbac5a070ab2239f
LinaGarcia24 <linavgarcia24@gmail.com>
2022-02-02 03:00:46 +0000

    Update subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,8 +106,8 @@ document.addEventListener("DOMContentLoaded", function() {
  }
 
  function displaySubtractQuestion(operand1, operand2) {
-     document.getElementById('operand1').textContent = operand1;
-     document.getElementById('operand2').textContent = operand2;
+     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+     document.getElementById('operand2').textContent = operand2 < operand1 ? operand2 : operand1;
      document.getElementById('operator').textContent = "-"
  }
 
